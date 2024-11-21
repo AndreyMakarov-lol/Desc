3e6ca25279f296e5e87325f4e9594705a43c573e
AndreyMakarov-lol <64976956+AndreyMakarov-lol@users.noreply.github.com>
2024-11-21 23:00:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ root.title('Моё первое приложение') # Задаем загол
 #root.iconbitmap(file="hyi.ico") # Установка иконки
 icon = PhotoImage(file="hyi.png") # Установка иконкой изображения
 root.iconphoto(False, icon)
-root.geometry("300x200+200+200") # Задаем размер окна + смещение окна относительно левого верхнего угла
+root.geometry("400x400+200+200") # Задаем размер окна + смещение окна относительно левого верхнего угла
 #root.update_idletasks() # метод необходим для отображения в обход root.mainloop() размера окна
 #print(root.geometry()) # отображение в консоли размера окна и отступов 
 #root.resizable(False, False) # запрет изменения размера окна
@@ -31,10 +31,11 @@ lable = Label(text="Хуя себе, смотри что могу") # Созда
 lable.pack() # размещаем метку в окне
 # Создаем кнопку, command=click_button отвечает за выполнение внешней функции
 btn1 = ttk.Button(text="Зашифровать", command=click_button) # Для отключения кнопки state=["disabled"]
-btn1.pack()
-
+btn1.pack(fill=X,padx=[100,50], pady=50, ipadx=20, ipady=20) # (expand=True) чтобы элемент занимал все свободное место
+# fill для заполнения контейнера BOTH Заполняет все, X и Y свои оси
+# anchor расположение элемента по углам и вершинам
+# Отступы элемента padxy
 root.protocol("WM_DELETE_WINDOW", finish) # перехват закрытия приложения
 
 root.mainloop() # запускаем цикл обработки событий окна
-
 #https://metanit.com/python/tkinter/1.2.php

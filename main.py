6b8341e1ab8f18f2f6ebd2eb60a1d433bb34eb47
Andrey_Makarov <64976956+AndreyMakarov-lol@users.noreply.github.com>
2024-11-21 19:35:05 +0300

    1

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from tkinter import *
+from tkinter import ttk
 
+# Подсчет количества кликов
+clicks = 0
+def click_button():
+    global clicks
+    clicks += 1
+    # Изменяем текст в кнопке
+    btn1['text'] = f"Кликов {clicks}"
 # Надо для отслеживания закрытия приложения
 def finish():
     root.destroy()  # ручное закрытие окна и всего приложения
@@ -11,7 +19,7 @@ root.title('Моё первое приложение') # Задаем загол
 #root.iconbitmap(file="hyi.ico") # Установка иконки
 icon = PhotoImage(file="hyi.png") # Установка иконкой изображения
 root.iconphoto(False, icon)
-#root.geometry("300x200+200+200") # Задаем размер окна + смещение окна относительно левого верхнего угла
+root.geometry("300x200+200+200") # Задаем размер окна + смещение окна относительно левого верхнего угла
 #root.update_idletasks() # метод необходим для отображения в обход root.mainloop() размера окна
 #print(root.geometry()) # отображение в консоли размера окна и отступов 
 #root.resizable(False, False) # запрет изменения размера окна
@@ -21,10 +29,12 @@ root.iconphoto(False, icon)
 
 lable = Label(text="Хуя себе, смотри что могу") # Создаем текст внутри окна
 lable.pack() # размещаем метку в окне
-
+# Создаем кнопку, command=click_button отвечает за выполнение внешней функции
+btn1 = ttk.Button(text="Зашифровать", command=click_button) # Для отключения кнопки state=["disabled"]
+btn1.pack()
 
 root.protocol("WM_DELETE_WINDOW", finish) # перехват закрытия приложения
 
 root.mainloop() # запускаем цикл обработки событий окна
 
-https://metanit.com/python/tkinter/1.2.php
+#https://metanit.com/python/tkinter/1.2.php
